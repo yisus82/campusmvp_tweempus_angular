55edff440171006e78d97612acab6cada554968c
yisus82 <djalma8@gmail.com>
2024-11-07 09:47:56 +0100

    Dashboard component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
 import { HeaderComponent } from './core/header/header.component';
 import { NavComponent } from './core/nav/nav.component';
 
 @Component({
   selector: 'tweempus-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavComponent],
+  imports: [RouterOutlet, HeaderComponent, NavComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
